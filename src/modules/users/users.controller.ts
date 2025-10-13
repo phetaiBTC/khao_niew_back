de75881244f2ad16dba2fd004a5a7efaeb07859c
uablor <uablauj76681809@gmail.com>
2025-10-13 14:35:05 +0700

    update user

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,31 +6,36 @@ import { PaginateDto } from 'src/common/dto/paginate.dto';
 import { Public } from 'src/common/decorator/auth.decorator';
 import { AuthProfile } from 'src/common/decorator/user.decorator';
 import { PayloadDto } from '../auth/dto/auth.dto';
+import { Roles } from 'src/common/decorator/role.decorator';
+import { EnumRole } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
+  @Roles(EnumRole.ADMIN, EnumRole.COMPANY)
   @Post()
-  create(@Body() createUserDto: CreateUserDto,@AuthProfile() user: PayloadDto) {
-    return this.usersService.create(createUserDto,user.company);
+  create(@Body() createUserDto: CreateUserDto, @AuthProfile() user: PayloadDto) {
+    return this.usersService.create(createUserDto, user.company);
   }
 
+  @Roles(EnumRole.ADMIN, EnumRole.COMPANY)
   @Get()
-  findAll(@Query() query:PaginateDto) {
+  findAll(@Query() query: PaginateDto) {
     return this.usersService.findAll(query);
   }
 
+  @Roles(EnumRole.ADMIN, EnumRole.COMPANY)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
-
+  @Roles(EnumRole.ADMIN, EnumRole.COMPANY)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
-
+  @Roles(EnumRole.ADMIN, EnumRole.COMPANY)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
